987cfa78e479409d79a0f2b6ef32c1935a321590
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-09-10 16:42:04 -0400

    Created rectangle + quad

--- experiments/Drawing-experiments/js/script.js
+++ experiments/Drawing-experiments/js/script.js
@@ -10,6 +10,20 @@ Here is a description of this template p5 project.
 // Description of setup() goes here.
 function setup() {
 
+  createCanvas(500,500);
+
+  background(200,200,200);
+
+  quad(60,50, 50,300, 150,380, 400,450);
+
+  rectMode(CENTER);
+  rect(250,250,100,100);
+  rect(250,250,80,80);
+  rect(250,250,60,60);
+
+
+
+
 }
 
 // draw()
