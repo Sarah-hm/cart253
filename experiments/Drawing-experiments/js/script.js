b4080fcf6bcc98e3b65c9d31267e8609bbc86ab3
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-09-10 17:04:41 -0400

    Created colors and transparency

--- experiments/Drawing-experiments/js/script.js
+++ experiments/Drawing-experiments/js/script.js
@@ -12,16 +12,20 @@ function setup() {
 
   createCanvas(500,500);
 
-  background(200,200,200);
+  background(255, 160, 43);
+  stroke(121, 247, 211)
 
+  fill(167,87,190);
+  noFill();
   quad(60,50, 50,300, 150,380, 400,450);
 
   rectMode(CENTER);
+  fill(167,87,210,125);
   rect(250,250,100,100);
+  fill(167,87,220);
   rect(250,250,80,80);
+  fill(167,87,240);
   rect(250,250,60,60);
-
-
 
 
 }
