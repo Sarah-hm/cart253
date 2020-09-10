bcec474e79ec7bf942513f218d9d8e77b9e45f8f
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-09-10 17:30:44 -0400

    Drawing experiments: add commenting

--- experiments/Drawing-experiments/js/script.js
+++ experiments/Drawing-experiments/js/script.js
@@ -9,16 +9,17 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
-
+//sets canvas background
   createCanvas(500,500);
-
   background(255, 160, 43);
-  stroke(121, 247, 211)
 
+//Background mint quad outline
+  stroke(121, 247, 211)
   fill(167,87,190);
   noFill();
   quad(60,50, 50,300, 150,380, 400,450);
 
+//Three middle rectangles
   rectMode(CENTER);
   fill(167,87,210,125);
   rect(250,250,100,100);
