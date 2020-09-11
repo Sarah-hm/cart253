9cadb0468819c97b61adf838f8c240eb677ef6e4
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-09-11 08:29:11 -0400

    02 Draw alien : created body, head and eyes

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -9,7 +9,36 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
-  background(128, 255, 20);
+
+  //Set up background
+  createCanvas(640,480);
+  background(255, 213, 184);
+
+  //Draw eyes
+  noStroke();
+  fill(172, 236, 213);
+  ellipse(200,150,50,100);
+  ellipse(435,150,50,100);
+    //white portions
+    fill(254, 251, 234);
+    ellipse(200,150,30,80);
+    ellipse(435,150,30,80)
+    //pupils
+    fill(66, 71, 71);
+    ellipse(200,150,15,35);
+    ellipse(435,150,15,35)
+    //reflection
+    fill(255,255,255);
+    ellipse(197,143,4,10)
+    ellipse(432,143,4,10);
+
+  //Draw body and head
+  noStroke();
+  fill(172, 236, 213);
+  ellipse(320,550, 200, 700);
+  ellipse(320,200,400,125);
+
+
 }
 
 // draw()
