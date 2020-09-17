0d91b46a8cbb1c28866daf099d00b238fe2388d2
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-09-17 18:29:16 -0400

    experiment-variables: learned about debugging

--- experiments/variables-experiments/js/script.js
+++ experiments/variables-experiments/js/script.js
@@ -5,28 +5,32 @@ Sarah Hontoy-Major
 Here is a description of this template p5 project.
 **************************************************/
 let backgroundShade = 0;
-
 let circle = {
-  x:0,
-  y:250,
-  size:250,
-  speed:2
+  x: 0,
+  y: 250,
+  size: 250,
+  speed: 2
 }
-
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-  createCanvas(500,500);
+  createCanvas(500, 500);
 }
-
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-  // backgroundShade = backgroundShade + 1
+  backgroundShade = backgroundShade + 1
   background(backgroundShade);
   circle.x += circle.speed
-  ellipse (circle.x,circle.y,circle.size);
+  ellipse(circle.x, circle.y, circle.size);
+
+
+  console.log(
+    `circle.x: ${circle.x}`,
+    `circle.size: ${circle.size}`,
+    `circle.speed:${circle.speed}`,
+    `circle.y: ${circle.y}`)
 
 }
