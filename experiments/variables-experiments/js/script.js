e443e277c1db2492054ab7513d7eb9fc1a095d1a
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-09-17 18:03:46 -0400

    experiment-variables: created js object

--- experiments/variables-experiments/js/script.js
+++ experiments/variables-experiments/js/script.js
@@ -5,13 +5,13 @@ Sarah Hontoy-Major
 Here is a description of this template p5 project.
 **************************************************/
 let backgroundShade = 0;
-let circleX = 0;
-let circleY = 250;
-let circleSize=25;
-let circleSpeed=2;
-let circleAcceleration=0.25;
-let circleOffset=300
-letcircleOffsetVariation=-100
+
+let circle = {
+  x:0,
+  y:250,
+  size:250,
+  speed:2
+}
 
 // setup()
 //
@@ -26,11 +26,7 @@ function setup() {
 function draw() {
   // backgroundShade = backgroundShade + 1
   background(backgroundShade);
-  circleX += circleSpeed
-  circleSpeed += circleAcceleration
-  ellipse (circleX,circleY,circleSize+300)
-  ellipse (circleX,circleY,circleSize+200)
-  ellipse (circleX,circleY,circleSize+100)
-  ellipse (circleX,circleY,circleSize);
+  circle.x += circle.speed
+  ellipse (circle.x,circle.y,circle.size);
 
 }
