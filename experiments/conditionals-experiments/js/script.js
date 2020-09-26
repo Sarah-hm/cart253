aefd0a3199398c21d4636a3121b83192c9f9e753
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-09-26 13:21:51 -0400

    conditionals-experiments: created movement

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -1,29 +1,54 @@
 let bg = {
-  r: 67,
-  g: 0,
-  b: 0,
+  r: 230,
+  g: 255,
+  b: 255,
 };
+
 let circle = {
   x: 250,
   y: 250,
   size: 100,
+  vx: 0,
+  vy: 0,
+  ax: 0,
+  ay: 0,
+  acceleration: 0.1,
+  maxSpeed: 4,
+  fill: {
+    r: 255,
+    g: 230,
+    b: 230,
+  },
 };
 
 function setup() {
   createCanvas(500, 500);
+  noStroke();
 }
 
 function draw() {
   background(bg.r, bg.g, bg.b);
 
+  if (mouseX < circle.x) {
+    circle.ax = -circle.acceleration;
+  } else {
+    circle.ax = circle.acceleration;
+  }
+
+  if (mouseY < circle.y) {
+    circle.ay = -circle.acceleration;
+  } else {
+    circle.ay = circle.acceleration;
+  }
+
+  circle.vx += circle.ax;
+  circle.vx = constrain(circle.vx, -circle.maxSpeed, circle.maxSpeed);
+  circle.vy += circle.ay;
+  circle.vy = constrain(circle.vy, -circle.maxSpeed, circle.maxSpeed);
+
+  circle.x += circle.vx;
+  circle.y += circle.vy;
+
+  fill(circle.fill.r, circle.fill.g, circle.fill.b);
   ellipse(circle.x, circle.y, circle.size);
-}
-
-function mouseWheel() {
-  circle.x = mouseX;
-  circle.y = mouseY;
-
-  bg.r = random(0, 255);
-  bg.g = random(0, 255);
-  bg.b = random(0, 255);
 }
