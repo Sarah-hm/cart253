6e8149a7e871cea2513222dafaf9f9f70ce5a258
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-09-26 15:05:20 -0400

    conditionals-experiments: transformed square (rotated, translated, scaled)

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -1,24 +1,13 @@
 let bg = {
-  r: 230,
-  g: 255,
-  b: 255,
+  r: 235,
+  b: 250,
+  g: 235,
 };
 
-let circle = {
-  x: 250,
-  y: 250,
-  size: 100,
-  vx: 0,
-  vy: 0,
-  ax: 0,
-  ay: 0,
-  acceleration: 0.1,
-  maxSpeed: 4,
-  fill: {
-    r: 255,
-    g: 230,
-    b: 230,
-  },
+let rectangle = {
+  angle: 0,
+  speed: 0.01,
+  rectScale: 0,
 };
 
 function setup() {
@@ -27,28 +16,16 @@ function setup() {
 }
 
 function draw() {
-  background(bg.r, bg.g, bg.b);
+  background(bg.r, bg.b, bg.g);
 
-  if (mouseX < circle.x) {
-    circle.ax = -circle.acceleration;
-  } else {
-    circle.ax = circle.acceleration;
-  }
-
-  if (mouseY < circle.y) {
-    circle.ay = -circle.acceleration;
-  } else {
-    circle.ay = circle.acceleration;
-  }
-
-  circle.vx += circle.ax;
-  circle.vx = constrain(circle.vx, -circle.maxSpeed, circle.maxSpeed);
-  circle.vy += circle.ay;
-  circle.vy = constrain(circle.vy, -circle.maxSpeed, circle.maxSpeed);
-
-  circle.x += circle.vx;
-  circle.y += circle.vy;
-
-  fill(circle.fill.r, circle.fill.g, circle.fill.b);
-  ellipse(circle.x, circle.y, circle.size);
+  push();
+  fill(255, 230, 230);
+  rectMode(CENTER);
+  translate(width / 2, height / 2);
+  rotate(rectangle.angle);
+  scale(rectangle.rectScale);
+  rect(0, 0, 100, 100);
+  rectangle.angle += rectangle.speed;
+  rectangle.rectScale += rectangle.speed;
+  pop();
 }
