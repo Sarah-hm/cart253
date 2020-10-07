11c07eaf48c5766e09fbcc15962fc0612c11bd04
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-10-06 22:01:06 -0400

    functions-experiments: created function parameters

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -1,49 +1,23 @@
-let circle = {
-  x: 0,
-  y: 250,
-  size: 100,
-  vx: 1,
-  vy: 0,
-};
-
 // Description of setup() goes here.
 function setup() {
   createCanvas(500, 500);
 }
 
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
   background(0);
-  move();
-  wrap();
-  display();
+
+  parallels(100, 100, 45, 1, 100, 1);
+  parallels(45, 40, 15, 2, 20, 6);
+  parallels(433, 217, 2, 7, 3, 20);
+  parallels(2, 456, 33, 1, 300, 30);
 }
 
-function display() {
-  fill(255, 0, 0);
-  ellipse(circle.x, circle.y, circle.size);
-}
-
-function wrap() {
-  if (circle.x > width) {
-    reset();
+function parallels(x, y, numLines, lineWidth, lineHeight, lineSpacing) {
+  for (let i = 0; i < numLines; i++) {
+    noStroke();
+    fill(255);
+    rectMode(CENTER);
+    rect(x, y, lineWidth, lineHeight);
+    x += lineSpacing;
   }
-}
-
-function move() {
-  circle.x += circle.vx;
-  circle.y += circle.vy;
-}
-
-function reset() {
-  circle.x = 0;
-  circle.vx += 1;
-  circle.vy -= 0.25;
-  circle.size += 10;
-}
-
-function mousePressed() {
-  reset();
 }
