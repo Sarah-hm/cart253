c4e26a645e1a4d96b345fd07c0e0fd00a171cf24
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-10-06 21:25:38 -0400

    functions-experiments : created functions

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -1,20 +1,49 @@
-/**************************************************
-Template p5 project
-Pippin Barr
+let circle = {
+  x: 0,
+  y: 250,
+  size: 100,
+  vx: 1,
+  vy: 0,
+};
 
-Here is a description of this template p5 project.
-**************************************************/
-
-// setup()
-//
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(500, 500);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
+  move();
+  wrap();
+  display();
+}
 
+function display() {
+  fill(255, 0, 0);
+  ellipse(circle.x, circle.y, circle.size);
+}
+
+function wrap() {
+  if (circle.x > width) {
+    reset();
+  }
+}
+
+function move() {
+  circle.x += circle.vx;
+  circle.y += circle.vy;
+}
+
+function reset() {
+  circle.x = 0;
+  circle.vx += 1;
+  circle.vy -= 0.25;
+  circle.size += 10;
+}
+
+function mousePressed() {
+  reset();
 }
