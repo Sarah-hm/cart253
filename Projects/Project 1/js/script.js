504a3984dd82a2b84d60e6397235443366e10317
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-10-11 16:20:09 -0400

    P1: added mock background, mock user

--- Projects/Project 1/js/script.js
+++ Projects/Project 1/js/script.js
@@ -1,20 +1,37 @@
 /**************************************************
-Template p5 project
-Pippin Barr
-
-Here is a description of this template p5 project.
+Project 1
+Sarah Hontoy-Major
 **************************************************/
+let bg = {
+  r: 0,
+  g: 255,
+  b: 0,
+};
+let user = {
+  x: 250,
+  y: 250,
+  size: 100,
+  // vx: 0,
+  // vy: 0,
+  // speed: 4,
+  image: undefined,
+};
 
-// setup()
-//
-// Description of setup() goes here.
+// ==== Download all images ====
+function preload() {
+  user.image = loadImage("assets/images/clown.png");
+}
+// ==== Set original background and universal aspects =====
 function setup() {
-
+  createCanvas(windowHeight, windowWidth);
+  background(bg.r, bg.g, bg.b);
+  rectMode(CENTER);
+  noStroke();
 }
 
-// draw()
-//
 // Description of draw() goes here.
 function draw() {
+  background(bg.r, bg.g, bg.b);
 
+  image(user.image, user.x, user.y, user.size, user.size);
 }
