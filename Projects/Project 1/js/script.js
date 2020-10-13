7dea7e8b5aff2612ba87376d8b413255874bfe3e
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-10-13 07:03:54 -0400

    P1: copied from 'experiments-p1' to p1. added new trash + collision make trash disappears

--- Projects/Project 1/js/script.js
+++ Projects/Project 1/js/script.js
@@ -3,6 +3,8 @@ Project 1; let's save the planet, one rubbish at a time
 Sarah Hontoy-Major
 **************************************************/
 "use strict";
+
+let trashImg = undefined;
 let bg = {
   r: 0,
   g: 255,
@@ -31,16 +33,39 @@ let rubbish = {
   state: `activated`, // can be activated or neutral
 };
 
+// First trash object
+let trash1 = {
+  x: 250,
+  y: 300,
+  size: 50,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  picked: false, // We want to track whether the user has picked the trash
+};
+
+// Second trash object
+let trash2 = {
+  x: 350,
+  y: 300,
+  size: 50,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  picked: false,
+};
+
 // ==== Download all images ====
 function preload() {
   user.image = loadImage("assets/images/clown.png");
   rubbish.image = loadImage("assets/images/star.png");
+  trashImg = loadImage("assets/images/star.png");
 }
 // ==== Set original background and universal aspects =====
 function setup() {
   createCanvas(windowHeight, windowWidth);
-  user.x = width / 2;
-  user.y = height / 2;
+  trashStartingPoint(trash1);
+  trashStartingPoint(trash2);
   rectMode(CENTER);
   noStroke();
 }
@@ -49,11 +74,27 @@ function setup() {
 function draw() {
   background(bg.r, bg.g, bg.b);
   usermove();
+
+  //checking if trash has been picked //
+  checkTrash(trash1);
+  checkTrash(trash2);
+  trashMove(trash1);
+  trashMove(trash2);
   rubbishMove();
   // checkrubbishOverlap();
   // wait5Secs();
   displayUser();
+
+  //display trashPile
+  displayTrash(trash1);
+  displayTrash(trash2);
+
   displayrubbish();
+}
+
+function trashStartingPoint(trash) {
+  trash.x = random(0, width);
+  trash.y = random(0, height);
 }
 
 // ====== make user move ======
@@ -74,6 +115,31 @@ function usermove() {
   }
   user.x = user.x + user.vx;
   user.y = user.y + user.vy;
+}
+
+// Checks if the user overlaps the trash1 object and eats it if so
+function checkTrash(trash) {
+  // We only want to check for an overlap if trash1 hasn't been picked yet
+  if (!trash.picked) {
+    let d = dist(user.x, user.y, trash.x, trash.y);
+    if (d < user.size / 2 + trash.size / 2) {
+      trash.picked = true;
+    }
+  }
+}
+
+function trashMove(trash) {
+  let change = random();
+  if (change < 0.1) {
+    trash.vx = random(-trash.speed, trash.speed);
+    trash.vy = random(-trash.speed, trash.speed);
+  }
+
+  trash.x += trash.vx;
+  trash.y += trash.vy;
+
+  trash.x = constrain(trash.x, 0, width);
+  trash.y = constrain(trash.y, 0, height);
 }
 
 function rubbishMove() {
@@ -112,6 +178,17 @@ function rubbishMove() {
 function displayUser() {
   image(user.image, user.x, user.y, user.size, user.size);
 }
+
+function displayTrash(trash) {
+  // Check if the trash is still available to be picked
+  if (!trash.picked) {
+    // Display the trash as its position and with its size
+    push();
+    image(trashImg, trash.x, trash.y, trash.size, trash.size);
+    pop();
+  }
+}
+
 function displayrubbish() {
   //Display rubbish 1 =====
   image(rubbish.image, rubbish.x, rubbish.y, rubbish.size, rubbish.size);
