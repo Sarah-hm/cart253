bd9f81d82d5ba69ac3b34a0e5402e89c3d928296
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-10-13 14:44:08 -0400

    experiments-P1: changed names, added 'for' loops

--- experiments/Experiments-Project1/js/script.js
+++ experiments/Experiments-Project1/js/script.js
@@ -1,66 +1,71 @@
 "use strict";
+let trashImg = undefined;
+function preload() {
+  trashImg = loadImage("assets/images/star.png");
+}
 
-let school = [];
-let schoolSize = 8;
+let trashPile = [];
+let trashPileSize = 8;
 
 function setup() {
   createCanvas(600, 600);
+  imageMode(CENTER);
 
-  // Create four fish, positioned randomly
-  for (let i = 0; i < schoolSize; i++) {
-    school[i] = createFish(random(0, width), random(0, height));
+  // Create four trash, positioned randomly
+  for (let i = 0; i < trashPileSize; i++) {
+    trashPile[i] = createTrash(random(0, width), random(0, height));
   }
 }
 
-// createFish(x,y)
-// Creates a new JavaScript Object describing a fish and returns it
-function createFish(x, y) {
-  let fish = {
+// createTrash(x,y)
+// Creates a new JavaScript Object describing a trash and returns it
+function createTrash(x, y) {
+  let trash = {
     x: x,
     y: y,
-    size: 50,
+    size: 100,
     vx: 0,
     vy: 0,
-    speed: 2,
+    speed: 4,
   };
-  return fish;
+  return trash;
 }
 
 // draw()
-// Moves and displays our fish
+// Moves and displays our trash
 function draw() {
   background(0);
 
-  for (let i = 0; i < schoolSize; i++) {
-    moveFish(school[i]);
-    displayFish(school[i]);
+  for (let i = 0; i < trashPileSize; i++) {
+    moveTrash(trashPile[i]);
+    displayTrash(trashPile[i]);
   }
 }
-// moveFish(fish)
-// Chooses whether the provided fish changes direction and moves it
-function moveFish(fish) {
+// moveTrash(trash)
+// Chooses whether the provided trash changes direction and moves it
+function moveTrash(trash) {
   // Choose whether to change direction
   let change = random(0, 1);
   if (change < 0.05) {
-    fish.vx = random(-fish.speed, fish.speed);
-    fish.vy = random(-fish.speed, fish.speed);
+    trash.vx = random(-trash.speed, trash.speed);
+    trash.vy = random(-trash.speed, trash.speed);
   }
 
-  // Move the fish
-  fish.x = fish.x + fish.vx;
-  fish.y = fish.y + fish.vy;
+  // Move the trash
+  trash.x = trash.x + trash.vx;
+  trash.y = trash.y + trash.vy;
 
-  // Constrain the fish to the canvas
-  fish.x = constrain(fish.x, 0, width);
-  fish.y = constrain(fish.y, 0, height);
+  // Constrain the trash to the canvas
+  trash.x = constrain(trash.x, 0, width);
+  trash.y = constrain(trash.y, 0, height);
 }
 
-// displayFish(fish)
-// Displays the provided fish on the canvas
-function displayFish(fish) {
+// displayTrash(trash)
+// Displays the provided trash on the canvas
+function displayTrash(trash) {
   push();
   fill(200, 100, 100);
   noStroke();
-  ellipse(fish.x, fish.y, fish.size);
+  image(trashImg, trash.x, trash.y, trash.size, trash.size);
   pop();
 }
