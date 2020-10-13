6255591cdcc721d06e30572904da9916d47fd7bd
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-10-13 07:28:40 -0400

    experiments-p1: created working array woot woot

--- experiments/Experiments-Project1/js/script.js
+++ experiments/Experiments-Project1/js/script.js
@@ -1,86 +1,66 @@
 "use strict";
-let trashImg = undefined;
 
-function preload() {
-  trashImg = loadImage("assets/images/star.png");
-}
-// Our user, to move with the mouse
-let user = {
-  x: 0,
-  y: 0,
-  size: 100,
-};
-
-// First trash object
-let trash1 = {
-  x: 250,
-  y: 300,
-  size: 50,
-  picked: false, // We want to track whether the user has picked the trash
-};
-
-// Second trash object
-let trash2 = {
-  x: 350,
-  y: 300,
-  size: 50,
-  picked: false,
-};
+let school = [];
+let schoolSize = 8;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  //set trash starting point
-  trash1.x = random(0, width);
-  trash1.y = random(0, height);
+  createCanvas(600, 600);
+
+  // Create four fish, positioned randomly
+  for (let i = 0; i < schoolSize; i++) {
+    school[i] = createFish(random(0, width), random(0, height));
+  }
 }
 
+// createFish(x,y)
+// Creates a new JavaScript Object describing a fish and returns it
+function createFish(x, y) {
+  let fish = {
+    x: x,
+    y: y,
+    size: 50,
+    vx: 0,
+    vy: 0,
+    speed: 2,
+  };
+  return fish;
+}
+
+// draw()
+// Moves and displays our fish
 function draw() {
   background(0);
 
-  // Move the user (with the mouse)
-  moveUser();
-
-  // Check whether the user has picked trash
-  checktrash(trash1);
-  checktrash(trash2);
-
-  // Display the user and trashs
-  displayUser();
-  displayTrash(trash1);
-  displayTrash(trash2);
-}
-
-// Sets the user position to the mouse position
-function moveUser() {
-  user.x = mouseX;
-  user.y = mouseY;
-}
-
-// Checks if the user overlaps the trash1 object and eats it if so
-function checktrash(trash) {
-  // We only want to check for an overlap if trash1 hasn't been picked yet
-  if (!trash.picked) {
-    let d = dist(user.x, user.y, trash.x, trash.y);
-    if (d < user.size / 2 + trash.size / 2) {
-      trash.picked = true;
-    }
+  for (let i = 0; i < schoolSize; i++) {
+    moveFish(school[i]);
+    displayFish(school[i]);
   }
 }
+// moveFish(fish)
+// Chooses whether the provided fish changes direction and moves it
+function moveFish(fish) {
+  // Choose whether to change direction
+  let change = random(0, 1);
+  if (change < 0.05) {
+    fish.vx = random(-fish.speed, fish.speed);
+    fish.vy = random(-fish.speed, fish.speed);
+  }
 
-// Draw the user as a circle
-function displayUser() {
+  // Move the fish
+  fish.x = fish.x + fish.vx;
+  fish.y = fish.y + fish.vy;
+
+  // Constrain the fish to the canvas
+  fish.x = constrain(fish.x, 0, width);
+  fish.y = constrain(fish.y, 0, height);
+}
+
+// displayFish(fish)
+// Displays the provided fish on the canvas
+function displayFish(fish) {
   push();
-  fill(255);
-  ellipse(user.x, user.y, user.size);
+  fill(200, 100, 100);
+  noStroke();
+  ellipse(fish.x, fish.y, fish.size);
   pop();
-}
-// Display the trash provided as a parameter
-function displayTrash(trash) {
-  // Check if the trash is still available to be picked
-  if (!trash.picked) {
-    // Display the trash as its position and with its size
-    push();
-    image(trashImg, trash.x, trash.y, trash.size, trash.size);
-    pop();
-  }
 }
