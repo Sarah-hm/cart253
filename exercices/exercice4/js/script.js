f13fadf43fbb3fd5a83688500f3818247b417b1c
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-10-22 19:46:48 -0400

    E4: copied aquarium experiment

--- exercices/exercice4/js/script.js
+++ exercices/exercice4/js/script.js
@@ -1,20 +1,69 @@
-/**************************************************
-Template p5 project
-Pippin Barr
+"use strict";
 
-Here is a description of this template p5 project.
-**************************************************/
+let school = [];
+let schoolSize = 4;
 
-// setup()
-//
-// Description of setup() goes here.
 function setup() {
+  createCanvas(600, 600);
 
+  for (let i = 0; i < schoolSize; i++) {
+    school[i] = createFish(random(0, width), random(0, height));
+  }
 }
 
-// draw()
-//
-// Description of draw() goes here.
-function draw() {
+// createFish(x,y)
+// Creates a new JavaScript Object describing a fish and returns it
+function createFish(x, y) {
+  let fish = {
+    x: x,
+    y: y,
+    size: 50,
+    vx: 0,
+    vy: 0,
+    speed: 2,
+  };
+  return fish;
+}
 
+function draw() {
+  background(0);
+
+  for (let i = 0; i < school.length; i++) {
+    moveFish(school[i]);
+    displayFish(school[i]);
+  }
+}
+
+// moveFish(fish)
+// Chooses whether the provided fish changes direction and moves it
+function moveFish(fish) {
+  // Choose whether to change direction
+  let change = random(0, 1);
+  if (change < 0.05) {
+    fish.vx = random(-fish.speed, fish.speed);
+    fish.vy = random(-fish.speed, fish.speed);
+  }
+
+  // Move the fish
+  fish.x = fish.x + fish.vx;
+  fish.y = fish.y + fish.vy;
+
+  // Constrain the fish to the canvas
+  fish.x = constrain(fish.x, 0, width);
+  fish.y = constrain(fish.y, 0, height);
+}
+
+// displayFish(fish)
+// Displays the provided fish on the canvas
+function displayFish(fish) {
+  push();
+  fill(200, 100, 100);
+  noStroke();
+  ellipse(fish.x, fish.y, fish.size);
+  pop();
+}
+
+function mousePressed() {
+  let fish = createFish(mouseX, mouseY);
+  school.push(fish);
 }
