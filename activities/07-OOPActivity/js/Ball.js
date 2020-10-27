ce826c26cc1b1d2a17c327db2e4384ef8cb4b618
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-10-27 08:04:38 -0400

    A7: fixed bouncing horizontal typo

--- activities/07-OOPActivity/js/Ball.js
+++ activities/07-OOPActivity/js/Ball.js
@@ -47,7 +47,7 @@ class Ball {
       //the closer it is to the edge, the more velocity it is going to add (or subtract) from its original velocity
       // dx = difference in x axis
       let dx = this.x - paddle.x;
-      this.vx += map(dx, paddle.width / 2, paddle.width / 2, -2, 2)
+      this.vx += map(dx, -paddle.width / 2, paddle.width / 2, -2, 2)
 
 
       this.vy = -this.vy;
