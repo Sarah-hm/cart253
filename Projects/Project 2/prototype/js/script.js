88e279c492205887d229fb2201fde1008ae56e64
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-11-05 17:22:35 -0500

    Created the activity 09 - inheritance

---
+++ Projects/Project 2/prototype/js/script.js
@@ -0,0 +1,21 @@
+/**************************************************
+Sarah Hontoy-Major - Project 02 Proposal; 
+
+This is my prototype to "A millenial's survival guide", my final project.
+This is my attempt to write the landing page of my game, the instruction page,
+as well as the first level. God have mercy on me.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
---
+++ activities/08-Inheritance/js/script.js
@@ -0,0 +1,18 @@
+/**************************************************
+This is the activity about inheritance;
+Sarah Hontoy-Major
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
