7fa2787f97d3831dbacb2f451730cd2af647b7d9
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-11-12 12:29:54 -0500

    Finished activity 09(sound), started exercice6(sound)

--- activities/09-sound/js/script.js
+++ activities/09-sound/js/script.js
@@ -1,20 +1,48 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Activity-09 : sound
+Sarah Hontoy-Major
 
-Here is a description of this template p5 project.
+He is the activity on sound and physic. woaw.
 **************************************************/
+"use strict"
+
+// the balls
+let balls = [];
+
+//F-minor
+let notes = [`F3`, `G3`, `Ab4`, `Bb4`, `C4`, `Db4`, `Eb4`, `F4`];
+
 
 // setup()
-//
-// Description of setup() goes here.
+//Creates canvas
 function setup() {
+  createCanvas(600, 600);
+  userStartAudio();
 
 }
 
 // draw()
-//
-// Description of draw() goes here.
-function draw() {
 
+function draw() {
+  background(0);
+
+  //You can use the same 'let ball' and put something inside of it in
+  //a new function later one because, by using 'let' in front of it,
+  //you let the program know this variable will only exist within those { }
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    ball.move();
+    ball.bounce();
+    ball.display();
+  }
+}
+
+function mousePressed() {
+  createBall(mouseX, mouseY)
+}
+
+function createBall(x, y) {
+  let note = random(notes);
+  let ball = new Ball(x, y, note);
+  balls.push(ball)
 }
